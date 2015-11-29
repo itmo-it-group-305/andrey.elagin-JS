576611efd67ec57a19bce5d8537aee3fdc7ed7da
Andreyelkin <daddyingrave@gmail.com>
2015-11-29 23:54:42 +0300

    Изменил readme, пробую юнит тесты

---
+++ homework/29.11/js/script.js
@@ -0,0 +1,61 @@
+/**
+ * Created by daddyingrave on 25.11.15.
+ */
+//Рекурсивная функция для вычисления степени числа
+//function recurse (x, y) {
+//    if (y != 1) {
+//        return x * recurse(x, y - 1);
+//    }
+//    else {
+//        return x;
+//    }
+//}
+//
+//console.log(recurse(2, 35));
+
+//var f = function factorial(n) {
+//    return n ? n*factorial(n-1) : 1;
+//};
+//
+//var g = f;  // скопировали ссылку на функцию-факториал в g
+//f = null;
+//
+//console.log( g() ); // 120, работает!
+
+//function g() {
+//    return 1;
+//}
+//
+//alert(g());
+
+//var age = prompt('Ваш возраст');
+//age = parseInt(age);
+//switch (age) {
+//    case  18:
+//        alert( 'Никогда не сработает' ); // результат prompt - строка, а не число
+//
+//    case "18": // вот так - сработает!
+//        alert( 'Вам 18 лет!' );
+//        break;
+//
+//    default:
+//        alert( 'Любое значение, не совпавшее с case' );
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
