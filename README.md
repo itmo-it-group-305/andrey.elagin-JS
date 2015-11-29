643a062478e13e7fd238fd2a8b4b65a596f31174
Andreyelkin <daddyingrave@gmail.com>
2015-11-30 01:47:48 +0300

    Задачи с learn.javascript.ru

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 <hr>
 <a href="//github.com/itmo-it-group-305/andrey.elagin-JS/tree/master/homework/24.11 Arrays">Домашняя работа от 24.11 (Массивы)</a><br>
 <hr>
-<a href="//github.com/itmo-it-group-305/andrey.elagin-JS/tree/master/homework/22.11 Cycles and conditional expressions">Домашняя работа от 22.11 (циклы и условные операторы)</a><br>
+<a href="//github.com/itmo-it-group-305/andrey.elagin-JS/tree/master/homework/22.11 Cycles and conditional expressions">Домашняя работа от 22.11 (Циклы и условные операторы)</a><br>
 <hr>
 <img src="https://leto49e.storage.yandex.net/rdisk/ce7c3c8d5d6676a0a0dcbbd0766b38c8d72d3d52367fbf97094ec503a554707b/inf/Or80_SQyKvnJOpEFtI_JBQmmCrE2soHbXetcaph-rf92NAArIBJsL3s4D8Nzos0O2Riq7guymIWtWK-kcYZYGQ==?uid=0&filename=800x600.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&rtoken=549f807ed28eb56bb717f179e0a15265&force_default=no&ycrid=na-09ce4aa2333bf26b2b61cfcf640b09cc-downloader3d" alt="darth">
 
--- homework/29.11/js/script.js
+++ homework/29.11/js/script.js
@@ -42,14 +42,69 @@
 //        alert( 'Любое значение, не совпавшее с case' );
 //}
 
+//Код ниже получает из массива строк новый массив, содержащий их длины:
+//
+//    var arr = ["Есть", "жизнь", "на", "Марсе"];
+//
+//var arrLength = [];
+//for (var i = 0; i < arr.length; i++) {
+//    arrLength[i] = arr[i].length;
+//}
+//
+//alert( arrLength ); // 4,5,2,5
+//Перепишите выделенный участок: уберите цикл, используйте вместо него метод map.
 
+//var arr = ["Есть", "жизнь", "на", "Марсе"];
+//
+//var arrLength = arr.map(function(name) {
+//    return name.length;
+//});
+//
+////var arrLength = [];
+////for (var i = 0; i < arr.length; i++) {
+////    arrLength[i] = arr[i].length;
+////}
+//
+//alert( arrLength ); // 4,5,2,5
 
+//На входе массив чисел, например: arr = [1,2,3,4,5].
+//Напишите функцию getSums(arr), которая возвращает массив его частичных сумм.
+//Иначе говоря, вызов getSums(arr) должен возвращать новый массив из такого же числа элементов,
+//в котором на каждой позиции должна быть сумма элементов arr до этой позиции включительно.
+//То есть: для arr = [ 1, 2, 3, 4, 5 ]
+//getSums( arr ) = [ 1, 1+2, 1+2+3, 1+2+3+4, 1+2+3+4+5 ] = [ 1, 3, 6, 10, 15 ]
+//Еще пример: getSums([-2,-1,0,1]) = [-2,-3,-3,-2].
+//Функция не должна модифицировать входной массив.
+//В решении используйте метод arr.reduce.
 
+//arr = [ 1, 2, 3, 4, 5 ];
+// todo не понял почему так работает
+//function getSums(arr) {
+//    var resultArr = [];
+//    arr.reduce(function(sum, current, i) {
+//        return resultArr[i] = sum + current;
+//    }, 0);
+//    return resultArr;
+//}
+//console.log(getSums(arr));
 
+arr = [ 1, 2, 3, 4, 5 ];
 
+//function getSumAgain (arr) {
+//    var result = [];
+//
+//    var totalArray = arr.reduce(function(sum, current) {
+//        result.push(sum);
+//        return sum + current;
+//    });
+//    result.push(totalArray);
+//
+//    return result;
+//}
+//
+//console.log(getSumAgain(arr));
 
-
-
+// todo изучить метод reduce/reduceRight
 
 
 
