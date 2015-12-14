24daaa1172cfeeb55492e8a0d87fe654d2a9ef9f
Andreyelkin <daddyingrave@gmail.com>
2015-12-14 18:55:07 +0300

    jquery test

---
+++ classwork/11.12 Шаблонизация/js/script (2).js
@@ -0,0 +1,30 @@
+var temp = '<button id="close">{{buttonX}}</button>' +
+    '<label for="name">{{{name}}}' +
+    '<input type="text" id="name">' +
+    '</label>' +
+    '<label for="phone">{{{phone}}}' +
+    '<input type="text" id="phone">' +
+    '</label>' +
+    '<label for="email">{{{mail}}}' +
+    '<input type="text" id="email">' +
+    '</label>' +
+    '<button class="submit">{{{buttonOk}}}</button>';
+
+var data = {
+
+};
+
+//var templ = "{{#repo}} " +
+//    "<li>{{name}}</li>" +
+//    "{{/repo}}";
+//
+//var dataNew = {
+//    "repo": [
+//    { "name": "Прости прощай" },
+//    { "name": "Динозавр" },
+//    { "name": "Я буду думать о тебе" }]
+//};
+//
+//var content = Mustache.render(templ, dataNew);
+//var elem = document.querySelector("ul");
+//elem.innerHTML = content;
